5c67130a008ffac0a17c3f93a9d599f4f7eda160
johannkrugell <johannkrugell@gmail.com>
2022-05-20 06:27:44 +1000

    ADD COMPUTERPLAY FUNCTION Add function computerPlay that compare the players selection and the random computer selection after converting bith to lowercase. Calculates the winner and returns a winning or losing message.
    On branch oneround
    Changes to be committed:
    modified:   js/script.js
    
    Changes not staged for commit:
    modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -9,8 +9,47 @@ function computerPlay() {
   
   /* select the element in the array where the array index number matches the 
   random number */
-  console.log(optionsRPS[arrayIndexNumber]);
+  console.log(optionsRPS[arrayIndexNumber].toString());
+  return optionsRPS[arrayIndexNumber].toString();
 }
 
-computerPlay();
+/* Function to play a round of rock, paper, scissors */
+ function playRound(playerSelection, computerSelection) {
+    /* downcase the players selection */
+    playerSelection = playerSelection.toString().toLowerCase();
+   
+    /* create correct case for player and computer selection to return on winning
+       or losing message */
+    let playerSelectionRightCase = 
+        playerSelection.charAt(0).toUpperCase().concat(playerSelection.slice(1));
+    
+    let computerSelectionRightCase = 
+        computerSelection.charAt(0).toUpperCase().concat(computerSelection.slice(1));
+   
+    /* compare user vs. computer selection and determine winner */
+    if (playerSelection === computerSelection){
+    /* if the player and computer have selected the same item display draw */
+      return "It is a draw";
 
+    /* Player wins */  
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return `You win! ${playerSelectionRightCase} beats ${computerSelectionRightCase}`;
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        return `You win! ${playerSelectionRightCase} beats ${computerSelectionRightCase}`;
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return `You win! ${playerSelectionRightCase} beats ${computerSelectionRightCase}`;
+    /* Computer wins */  
+    } else if (computerSelection === "rock" && playerSelection === "scissors"){
+        return `You lose! ${computerSelectionRightCase} beats ${playerSelectionRightCase}`;
+    } else if (computerSelection === "paper" && playerSelection === "rock"){
+        return `You lose! ${computerSelectionRightCase} beats ${playerSelectionRightCase}`;
+    } else if (computerSelection === "scissors" && playerSelection === "paper"){
+        return `You lose! ${computerSelectionRightCase} beats ${playerSelectionRightCase}`; 
+  } 
+}
+
+ /* Call round of rock, paper, scissors */
+
+ let game = playRound(prompt("Please enter selection"),computerPlay());
+
+ alert(game);
