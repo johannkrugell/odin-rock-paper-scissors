793114b853aae2a28e849428c1236cc5d5a07dd3
johannkrugell <johannkrugell@gmail.com>
2022-06-15 07:10:57 +1000

    REFACTOR ADD GAME SECTION Updated the fucntion to calculated the winner and that is called when five rounds have been played. Added all functionality to update the DOM Tree with the selection and results. Refactored coded in the various section to decrease repition. Added timeout section to display and update the DOM tree in the correct order.
    On branch rps-ui
    Changes to be committed:
    new file:   css/styles.css
    new file:   img/artificial-intelligence-gdbe861b7a_640.png
    new file:   img/artificial-intelligence-gdbe861b7a_640.png:Zone.Identifier
    modified:   index.html
    modified:   js/script.js
    deleted:    styles.css

--- js/script.js
+++ js/script.js
@@ -1,144 +1,229 @@
-// event listeners
-const playerClicks = document.querySelectorAll('button'); //button event listeners
+// add event listeners to the options Rock, Paper or Scissors
+const playerClicks = document.querySelectorAll('button'); 
 playerClicks.forEach(click => {click.addEventListener('click', playerClick, true)});
 
-// variables update case
+// set rounds and player and computer scores to zero
 let countRounds = 0; 
-let playerScore = 0;  // set player score to zero 
+let playerScore = 0;  
 let computerScore = 0;
 
+/* The DOM elements will display the score during 
+   the round the element values are updating using 
+   updateScoreElement()
+*/
+const playerScoreElement = document.createElement("p");
+playerScoreElement.setAttribute("id","playerScore");
+const computerScoreElement = document.createElement("p");
+computerScoreElement.setAttribute("id","computerScore");
 
-// capture player selection value
+const calcWinner = document.createElement("p");
+calcWinner.setAttribute("id", "calcWinner");
+
+/* Set the number of seconds before the selection, round results or game results
+   are displayed
+*/
+const outputTimeWinner = 5000;
+const outputPlayerSelectionTime = 3000;
+const outputComputerSelectionTime = 5000;
+const outputRoundResultTime = 7000;
+const outputScoreTime = 9000;
+
+
+/* When a player selects an options
+   the number of rounds are incremented by 1
+   previous elements in the DOM are removed
+   the players selection value is recorded and assigned to playerSelection
+   the computers selection value is recored
+   both computerSelection and playerSelection values are passed to playRound
+*/
 function playerClick(e) {
-  if (countRounds < 4){
-      countRounds += 1;
-      e.target.innerText; //playerslection valuerock, paper or scissors
-      playerSelection = e.target.innerText;
-      computerPlay(); //call function to capture computer selection  
-      playRound();
-      
-      return playerSelection
-    }
-  playRound();  
-  countRounds += 1;
-  calcWinner(); 
-  playerScore = 0;
-  computerScore = 0;
-  countRounds = 0;  
+  countRounds += 1; 
+  if (countRounds <= 5) {  
+    removeElement(); 
+    removeRoundResult(); 
+    e.target.innerText;  
+    playerSelection = e.target.innerText; 
+    computerPlay(); 
+    playRound(); 
+    return playerSelection
+  }
 }   
 
-function computerPlay() {
-  let optionsRPS = ["rock","paper", "scissors"]; //rock,paper,scissors array
-  let arrayIndexNumber = Math.floor(Math.random() * 3); //random number == array index 
+/* The computers selection is created by randomly 
+   selecting a number between 1 and 3
+   the number is matched to the array index number to return that
+   value in the array
+*/
+function computerPlay() { 
+  let optionsRPS = ["rock","paper", "scissors"]; 
+  let arrayIndexNumber = Math.floor(Math.random() * 3);  
   computerSelection = optionsRPS[arrayIndexNumber].toString(); 
   return computerSelection;
 }
 
-function rightCase(selection) {
+// Update the case to show correctly in the DOM element
+function rightCase(selection) { 
   selection = selection.charAt(0).toUpperCase().concat(selection.slice(1));
   return selection;
 }
 
-function lowerCase(selection) {
+/* Downcase the selection to be able to compare 
+   the player and computer selections
+*/
+function lowerCase(selection) { 
   selection = selection.toString().toLowerCase();
   return selection;
 }
 
-function createElement(selection){
-  let outputSelection = document.createElement("p");
-  outputSelection.setAttribute("id","roundSelection");
-  outputSelection.innerText = `You have selected ${selection}`;
-  document.body.appendChild(outputSelection);
+/* Each time a player/computer makes a selection the selection
+   div is found and updated with the player/computers selection   
+*/
+function createElement(selection, selectionType) {
+  const outputSelection = document.getElementById("selection"); 
+  const updateSelection = document.createElement("p");
+  updateSelection.setAttribute("id","roundSelection");
+  if (selectionType == "player") {
+    updateSelection.innerText = `You have selected ${selection}`;
+  }
+  else {
+    updateSelection.innerText = `I have selected ${selection}`;
+  }
+    outputSelection.appendChild(updateSelection);
 }
 
-function removeElement(){
+/* Removes the elements create in prior rounds that displayed 
+   the players/computers selections 
+*/
+function removeElement() { 
   const element = document.querySelectorAll("#roundSelection");
-  for (i=0; i <element.length; i++) {
+  for (i = 0; i < element.length; i++) {
     element[i].remove();  
   }
 }
 
-function playRound() { //play round Rock, paper, scissors
-  
-  console.log(`Rounds:${countRounds}`);
-  
-  playerSelection = lowerCase(playerSelection) //downcase the players selection
-  let playerSelectionRightCase = rightCase(playerSelection); //correct case
-  let computerSelectionRightCase = rightCase(computerSelection); //correct case 
-
-  createElement(playerSelectionRightCase);
-  createElement(computerSelectionRightCase);
-
-        console.log(playerSelection);
-        console.log(computerSelection);
-        
-    /* compare user vs. computer selection and determine winner */
-    
-    if (playerSelection === computerSelection){
-        alert("It is a draw"); // player and computer selection match; display draw
-        result = 2; 
-        playerScore +=0;
-        computerScore += 0;
-        removeElement(); 
-        return result;
-    }
-    /* Player wins */
-    else if (playerSelectionRightCase === "rock" && computerSelection === "scissors" || 
-             playerSelection === "paper" && computerSelection === "rock" || 
-             playerSelection === "scissors" && computerSelection === "paper"){
-            alert(`You win! ${playerSelectionRightCase} beats ${computerSelectionRightCase}`);
-            playerScore += 1; 
-            removeElement();
-            console.log(`Player Score update: ${playerScore}`);
-            return playerScore;
-        }     
-    /* Computer wins */
-    else if (computerSelection === "rock" && playerSelection === "scissors" || 
-             computerSelection === "paper" && playerSelection === "rock" || 
-             computerSelection === "scissors" && playerSelection === "paper"){
-            alert(`You lose! ${computerSelectionRightCase} beats ${playerSelectionRightCase}`);
-            computerScore += 1; 
-            removeElement();
-            console.log(`Computer Score update: ${computerScore}`);
-            return computerScore;
-        } 
-        
+/* If it is the first round add the score element to the DOM tree
+   subsequent rounds update the score element value. If the last
+   round has been played calculate who the winner is
+*/
+function updateScoreElement() {
+  if (countRounds == 1){
+    document.getElementById("scores").appendChild(playerScoreElement);
+    document.getElementById("scores").appendChild(computerScoreElement);
+    updateScoreElementValue();
+  } 
+  else if (countRounds < 5 ) {
+    updateScoreElementValue();
+  } 
+  else if (countRounds === 5) {
+    updateScoreElementValue();
+    setTimeout(function() { createElementWinner() }, outputTimeWinner);  
+  }
 }
 
-/* Function to play a game 
-function game(){
-    console.log("call game function")
-    playerScore = parseInt(0);  // set player score to zero 
-    computerScore =parseInt(0); // set computer score to zero
-       
-    for (let i = 0; i < 5; i++){ // play 5 rounds 
-        console.log("in loop");
-        console.log(i);
-        playRound();
-        
-        let result = 0;
-               // call playRound function   
-        if (result === 1) {     // Check the result of the round; calc the score
-            playerScore += 1;
-            console.log(result);
-        }else if (result === 0){
-            computerScore += 1;
-        }else if (result === 2){
-            playerScore +=0;
-            computerScore += 0;
-        }  
-    console.log(playerScore);
+function updateScoreElementValue() {
+  playerScoreElement.innerText = `Your Score: ${playerScore}`;
+  computerScoreElement.innerText = `My Score: ${computerScore}`;
+}
 
-    };*/
+function removeScoreElement() {
+  document.getElementById("playerScore").remove();
+  document.getElementById("computerScore").remove();
+}
 
-    /* after 5 rounds determine the winner */ 
-function calcWinner() {
+/* Function is called from the playRound to display the message if the player 
+   or computer has won or if it is a draw. after displaying the result if the 
+   player clicks for another round the result is removed and a new roundResult is displayed
+*/
+function displayRoundResult(computerSelectionRightCase,         
+                            playerSelectionRightCase, roundResult) {
+  let result = document.createElement("p");
+  result.setAttribute("id","result");
+  switch (roundResult) {
+    case "draw":
+      result.innerText = `It is a draw`;
+      break;
+    case "playerWins":
+      result.innerText = `You win! ${playerSelectionRightCase} beats ${computerSelectionRightCase}`;  
+      break;
+    case "computerWins":
+      result.innerText = `You lose! ${computerSelectionRightCase} beats ${playerSelectionRightCase}`;
+      break;
+  }
+  document.getElementById("roundResult").appendChild(result);
+}
+
+function removeRoundResult() { 
+  if (countRounds > 1) {
+    document.getElementById("result").remove();
+  }
+}
+
+/* The function is called to calculate the winner and update the DOM tree
+   with the result. The function is called when the number of rounds played
+   equals five rounds
+*/
+function createElementWinner(){
   if (playerScore > computerScore ){
-    alert(`Player wins. Player score: ${playerScore} Computer score: ${computerScore}`);
+    calcWinner.innerText =`You win! ${playerScore} ${computerScore}`;
   } else if (computerScore > playerScore){
-    alert(`Computer wins. Player score: ${playerScore} Computer score: ${computerScore}`);
+    calcWinner.innerText = `You lose! ${playerScore} ${computerScore}`;
   } else if (playerScore === computerScore){
-    alert(`It is a draw. Player score: ${playerScore} Computer score: ${computerScore}`);
-    }       
-} 
+    calcWinner.innerText = `It is a draw! ${playerScore} ${computerScore}`;
+  }
+  document.getElementById("winner").appendChild(calcWinner) 
+}
+
+function playRound() { 
+  console.log(`Rounds:${countRounds}`);
+  // set case
+  playerSelection = lowerCase(playerSelection) 
+  let playerSelectionRightCase = rightCase(playerSelection); 
+  let computerSelectionRightCase = rightCase(computerSelection);  
+  
+  // create elements to output player and computer selections
+  setTimeout(function() {
+    createElement(playerSelectionRightCase,"player");
+  },outputPlayerSelectionTime);
+ 
+  setTimeout(function() {
+    createElement(computerSelectionRightCase,"computer");
+  }, outputComputerSelectionTime);
+       
+  console.log(playerSelection);
+  console.log(computerSelection);
+        
+  /* compare user vs. computer selection and determine winner */
     
+  if (playerSelection === computerSelection) {
+    playerScore +=0;
+    computerScore += 0;
+    roundResult = "draw";
+    setTimeout(() => { displayRoundResult(computerSelectionRightCase,       
+                                      playerSelectionRightCase,roundResult);  
+    }, outputRoundResultTime);
+    setTimeout(() => { updateScoreElement() }, outputScoreTime);   
+  }
+  else if (playerSelection === "rock" && computerSelection === "scissors" || 
+           playerSelection === "paper" && computerSelection === "rock" || 
+           playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore += 1; 
+    roundResult = "playerWins"
+    setTimeout(() => { displayRoundResult(computerSelectionRightCase,     
+                                        playerSelectionRightCase, roundResult);
+    }, outputRoundResultTime)      
+    setTimeout(() => { updateScoreElement() }, outputScoreTime)
+    console.log(`Player Score update: ${playerScore}`);
+  }     
+
+  else if (computerSelection === "rock" && playerSelection === "scissors" || 
+          computerSelection === "paper" && playerSelection === "rock" || 
+          computerSelection === "scissors" && playerSelection === "paper") {               
+    computerScore += 1; 
+    roundResult = "computerWins";
+    setTimeout(() => { displayRoundResult(computerSelectionRightCase, 
+                                        playerSelectionRightCase, roundResult);
+    }, outputRoundResultTime);
+    setTimeout(() => { updateScoreElement() }, outputScoreTime);    
+        console.log(`Computer Score update: ${computerScore}`);
+  }     
+}
